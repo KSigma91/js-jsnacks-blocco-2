cd9d89a28d71729fa07f5ee0239144e1d8ae86ba
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-12 20:28:03 +0200

    Jsnack2 ?

--- Jsnack2/js/script.js
+++ Jsnack2/js/script.js
@@ -1,21 +1,21 @@
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
 // qui saranno visualizzati i numeri
-const myNum = [];
+const listaNumeri = [];
 
-const totUser = 0;
-debugger;
+let sommaNumeri = 0;
+
 // faccio la somma dei numeri inseriti
 do {
-    // chiedo all'utente di inserire i numeri
-    let numUser = parseInt(prompt("Inserisci il numero: "));
+    // chiedo all'utente di inserire un numero
+    let numeroUtente = parseInt(prompt("Inserisci il numero: "));
+    // stampo il numero selezionato dall'utente
+    listaNumeri.push(numeroUtente);
 
-    myNum.push(numUser);
+    sommaNumeri += numeroUtente;
 
-    totUser += parseInt(numUser);
+    console.log(numeroUtente);
 
-    console.log(myNum);
+} while (sommaNumeri < 50)
 
-} while (totUser < 50)
-
-// let result = document.getElementById("mio_id").value;
+const somma = document.getElementById("mio_id").innerHTML = sommaNumeri;
