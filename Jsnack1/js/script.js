50ea2a08ab609224b2842e9a9b87ca7f364fee28
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-14 00:03:02 +0200

    Jsnack1 corretto

--- Jsnack1/js/script.js
+++ Jsnack1/js/script.js
@@ -1,16 +1,10 @@
 // qui verranno visualizzati i numeri randomici
-let numRandom = [];
-
-const myNum = document.getElementById("mio_id");
+const listaNumeri = [];
+const numeriUtente = document.getElementById("mio_id");
 
 // genero i numeri randomici
-while (numRandom.length < 10) {
-
-    let numGen = Math.floor(Math.random() * 100) + 1;
-
-    if (numRandom.includes(numGen) == false) {
-        numRandom.push(numGen);
-    }
-
-    console.log(numGen);
+for (i = 0; i < 10; i++){
+    listaNumeri.push(Math.floor(Math.random() * 100) + 1);   
 }
+numeriUtente.append(listaNumeri);
+console.log(listaNumeri);
